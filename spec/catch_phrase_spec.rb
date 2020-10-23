52d66dbfe13386ceadc20f55e955e77e61b9a896
Sean <seanvkamp@gmail.com>
2020-10-22 17:33:11 -0700

    commit tree e65ae7e0cc94b5a6a14c50b917fd62a4893aef57

--- spec/catch_phrase_spec.rb
+++ spec/catch_phrase_spec.rb
@@ -1,8 +1,4 @@
-require "spec_helper"
-
-describe "#catch_phrase" do 
-  it "puts out a catch phrase" do 
-    phrase = "It's-a me, Mario!"
-    expect{catch_phrase}.to output("It's-a me, Mario!\n").to_stdout
-  end
+def catch_phrase(phrase)
+puts "It's-a me, Mario!"
+return "#{phrase}"
 end
